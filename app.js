f4d37058d596d5063641d081a2d7ee695bb9ff72
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-11-29 10:07:03 -0700

    added delete request to app.js and querries

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const queries = require('./queries')
+var cors = require('cors')
 
 
 app.get('/', (req, res) => {
@@ -9,13 +10,18 @@ app.get('/', (req, res) => {
     
 })
 
-// app.get('/:id', (req, res) => {
-//     queries.getById(req.params.id).then(students => res.send(students))
-// })
+app.get('/:id', (req, res) => {
+    queries.getById(req.params.id).then(students => res.send(students))
+})
 
-// app.post('/', (req, res) => {
-//     queries.createStudent(req.body).then(students => res.send(students))
-// })
+app.post('/', (req, res) => {
+    queries.createStudent(req.body).then(students => res.send(students))
+})
+
+app.delete('/:id', (req, res) => {
+    queries.deleteStudent(request.params.id).then(res.sendStatus(204))
+})
+
 
 
 
--- queries.js
+++ queries.js
@@ -3,19 +3,20 @@ const database = require('./database-connection')
 module.exports = {
     listAll(){
           return database('students')
+    },
+    getById(id) {
+        return db('students').where('id', id)
+        }, 
+    createStudent(newStudent) {
+        return db('students').insert(newStudent).returning('*')
+        },
+    deleteStudent(id){
+        return db('students')
+            .where('id', id)
+            .delete()
+          }
     }
-  }
+    
+  
 
 
-  //notes from lecture
-//   module.exports = {
-//       getAll() {
-//           return db('students')
-//       },
-//       getById(id) {
-//           return db('students').where('id', id)
-//       }, 
-        // createStudent(newStudent) {
-        //     return db('students').insert(newStudent).returning('*')
-        // }
-//   }
